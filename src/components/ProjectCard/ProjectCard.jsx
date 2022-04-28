e58d16afe1d82f74fa2fc6527faf3c1a6e5b597d
Jordan <63085677+jordantan2@users.noreply.github.com>
2022-04-28 17:00:33 +0800

    Fixed crowdfunding

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./ProjectCard.css";
+
+function ProjectCard({ projectData }) {
+    return (
+        <div className="project-card">
+            <Link to={`/project/${projectData.id}`}>
+                <img src={projectData.image} alt="the project" />
+                <h3>{projectData.title}</h3>
+            </Link>
+        </div>
+    );
+}
+
+export default ProjectCard;
